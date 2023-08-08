a188d6e3b1f570157570cfb9d49a5a619ebbe9f9
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-08-08 21:53:13 +0300

    meeting_2

---
+++ src/meeting_02/CelciusToFahrenheit.java
@@ -0,0 +1,21 @@
+package meeting_02;
+
+public class CelciusToFahrenheit {
+    public static void main(String[] args) {
+        
+    } {
+
+        //Farenheit {F}= Temperature in degrees Celcius{C}+9/5+32
+        System.out.print("30 Celcius is:");
+        System.out.print(30.0 * (9.0 / 5.0) + 32.0);
+        System.out.println(" fahrenheit");
+
+
+        //Celcius{°C} = (°F - 32) × 5/9
+        System.out.print("86 Farenheit is:");
+        System.out.print((86.0 - 32.0) * (5.0 / 9.0));
+        System.out.println(" celcius");
+
+    }
+
+}
---
+++ src/meeting_02/DenemeTeam.java
@@ -0,0 +1,8 @@
+package meeting_02;
+
+public class DenemeTeam {
+    public static void main(String[] args){
+        System.out.println("Hello.World!");
+    }
+
+}
