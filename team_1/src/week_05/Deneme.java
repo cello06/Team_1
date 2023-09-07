28d3d6fc7337d6301f3e3162969984a6f22dd5ac
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-07 21:09:46 +0300

    team studies

---
+++ team_1/src/week_05/Deneme.java
@@ -0,0 +1,26 @@
+package week_05;
+
+
+import java.util.Scanner;
+
+public class Deneme {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of lines : ");
+        int numberOfLine = input.nextInt();
+
+        for(int i = 1; i<=numberOfLine;i++){
+            for(int j=1;j<=numberOfLine-i;j++){
+                System.out.print("  ");
+            }
+            for(int k=i;k>=1;k--){
+                System.out.print(k+" ");
+            }
+            for(int l=2;l<=i;l++){
+                System.out.print(l+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ team_1/src/week_05/Palindrome.java
@@ -0,0 +1,36 @@
+package week_05;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean isPalindrome = false;
+        System.out.print("Enter a string : ");
+        String yourStr = input.nextLine();
+        yourStr=yourStr.toUpperCase();
+
+        int firstIndexOfString = 0;
+        int lastIndexOfString = yourStr.length()-1;
+
+        while(firstIndexOfString<lastIndexOfString){
+
+
+            if(yourStr.charAt(firstIndexOfString)== yourStr.charAt(lastIndexOfString)){
+                isPalindrome=true;
+            }else{
+                isPalindrome=false;
+                break;
+            }
+
+            firstIndexOfString++;
+            lastIndexOfString--;
+        }
+
+        System.out.print("is that a palindrome : "+isPalindrome);
+
+
+
+
+    }
+}
